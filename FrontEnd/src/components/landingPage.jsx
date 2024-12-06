ca1a9122e114fc776e6b97cff9c24b9d8550732e
Anas Magdy <anasmagdy950@gmail.com>
2024-12-06 19:56:51 +0200

    fix-Unintended change in landing page

--- FrontEnd/src/components/landingPage.jsx
+++ FrontEnd/src/components/landingPage.jsx
@@ -11,7 +11,7 @@ function LandingPage() {
             <span>LIFTA</span>
             <img
               className="object-cover h-10 w-14"
-              src="c"
+              src="src/assets/logo.png"
               alt="logo"
             />
           </div>
