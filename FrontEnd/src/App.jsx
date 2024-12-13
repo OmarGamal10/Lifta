0e8a2db7b7c462cc0bf6a8aeb73e35cdca47f92c
shady-2004 <shadyshiko2004@gmail.com>
2024-12-13 15:14:39 +0200

    fix: sth in userType form

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -3,7 +3,7 @@ import { React } from "react";
 import IngForm from "./components/coach/IngredientForm.jsx";
 import PkgForm from "./components/coach/packageForm.jsx";
 import ExerciseForm from "./components/coach/exerciseForm.jsx";
-import UserProfile from "./components/userProfile.jsx"
+import UserProfile from "./components/userProfile.jsx";
 import Form from "./components/form";
 import FormTrainee from "./components/trainee/formTrainee";
 import LoginForm from "./components/loginForm";
@@ -19,9 +19,6 @@ import { twMerge } from "tailwind-merge";
 import "primeicons/primeicons.css";
 import { PackageDashboard } from "./components/packageDashboard.jsx";
 import { SubscriptionRequestCard } from "./components/coach/subscriptionRequestCard.jsx";
-import IngForm from "./components/coach/IngredientForm.jsx";
-import PkgForm from "./components/coach/packageForm.jsx";
-import ExerciseForm from "./components/coach/exerciseForm.jsx";
 import { SubReqDashboard } from "./components/coach/subReqDashboard.jsx";
 import { TraineesList } from "./components/admin/traineesList.jsx";
 import { CoachesList } from "./components/admin/coachesList.jsx";
@@ -29,7 +26,7 @@ import { CoachesList } from "./components/admin/coachesList.jsx";
 function App() {
   return (
     <div>
-      <PrimeReactProvider value={{  pt: Tailwind  }}>
+      <PrimeReactProvider value={{ pt: Tailwind }}>
         <PackageDashboard />
       </PrimeReactProvider>
     </div>
