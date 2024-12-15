7ad0719a9137dfef8fde81d8a6d57faae9ce56cb
OmarGamal10 <ogamal1011@gmail.com>
2024-12-15 18:19:31 +0200

    Finalized chat and handled expired/active

--- server/models/messageModel.js
+++ server/models/messageModel.js
@@ -6,15 +6,22 @@ exports.getAllMessages = async () => {
 };
 
 exports.getMessagesByChatId = async (chatId) => {
-  const sender_id = chatId.sender_id;
-  const receiver_id = chatId.receiver_id;
-  const query = `select * from lifta_schema.message where (sender_id = $1 and receiver_id = $2) or (sender_id = $2 and receiver_id = $1) order by time`;
-  return (await db.query(query, [sender_id, receiver_id])).rows;
+  const { sender_id, receiver_id, subscription_id } = chatId;
+  const query = `select * from lifta_schema.message where (sender_id = $1 and receiver_id = $2 and subscription_id = $3) or (sender_id = $2 and receiver_id = $1 and subscription_id=$3) order by time`;
+  return (await db.query(query, [sender_id, receiver_id, subscription_id]))
+    .rows;
 };
 
 exports.createMessage = async (message) => {
-  const { sender_id, receiver_id, content, time } = message;
-  const query = `insert into lifta_schema.message (sender_id, receiver_id, content, time,is_read) values ($1, $2, $3, $4, $5 ) returning *`;
-  return (await db.query(query, [sender_id, receiver_id, content, time, false]))
-    .rows[0];
+  const { sender_id, receiver_id, content, time, subscription_id } = message;
+  const query = `insert into lifta_schema.message (sender_id, receiver_id, content, time, subscription_id) values ($1, $2, $3, $4, $5) returning *`;
+  return (
+    await db.query(query, [
+      sender_id,
+      receiver_id,
+      content,
+      time,
+      subscription_id,
+    ])
+  ).rows[0];
 };
